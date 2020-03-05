9745f3e69c3cccd2ce3c537c4efb1d64892e398c
Виктория <55043240+zzomg@users.noreply.github.com>
2020-03-05 19:02:52 +0700

    Update lab7.c

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -73,7 +73,6 @@ int main()
 
     int k = 0;
     int offset = 0;
-    printf("Writing output:\n");
     for(int bytes = 0; bytes < mapping->fsize; ++bytes) {
         char c;
         if ((c = mapping->dataPtr[bytes]) == '\n') {
@@ -85,12 +84,12 @@ int main()
         }
     }
 
-    for(int i = 0; i < STOR_SIZE && lines[i].line_offset != -1; ++i) {
+    /*for(int i = 0; i < STOR_SIZE && lines[i].line_offset != -1; ++i) {
         printf("line_num: %d | line_len: %d | line_offset: %d\n", 
             lines[i].line_num,
             lines[i].line_len,
             lines[i].line_offset);
-    }
+    }*/
 
     int num_of_lines = 0;
 	for (int i = 0; i < STOR_SIZE && lines[i].line_num != 0; ++i) {
@@ -102,15 +101,17 @@ int main()
 	fds[0].fd = 0;
 	fds[0].events = POLLIN;
 
-	ret = poll(fds, 1, TIMEOUT * 1000);
-	scanf("%d", &line_n);
+	while((ret = poll(fds, 1, TIMEOUT * 1000))) {
+	    if(scanf("%d", &line_n)) break;
+    }
 
 	if (ret == -1) {
 		perror("Error: poll\n");
 		return -1;
 	}
 	else if (ret == 0) {
-		printf("Sorry, %d seconds expired. Please, try again\n", TIMEOUT);
+		printf("Sorry, %d seconds expired. Please, try again.\n", TIMEOUT);
+        printf("Writing output:\n");
 		system("cat input.txt");
 		return 0;
 	}
