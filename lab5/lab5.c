bdc060dfd1687a708b4f5ce27c54595da68b87db
Виктория <55043240+zzomg@users.noreply.github.com>
2020-06-27 15:34:38 +0700

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,81 +1,124 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <fcntl.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/poll.h>
+#include <ctype.h>
+#include <string.h>
+
+#define TIMEOUT 5 //n of sec to wait
 
 struct newLines
 {
-	int line_offset;
-	int line_len;
-	int line_num;
+    int line_offset;
+    int line_len;
+    int line_num;
 };
 
 int main()
 {
-	int file = open("input.txt", O_RDONLY);
+    int file = open("input.txt", O_RDONLY);
+    if(file < 0) {
+        perror("File opening failed\n");
+        return -1;
+    }
 
-	char buf;
-	int offset = 0;
-	int line_n = 0;
+    struct pollfd fds[1];
 
-	struct newLines lines[1024];
-	for (int i = 0; i < 1024; ++i) {
-		lines[i].line_len = 0;
-		lines[i].line_offset = -1;
-		lines[i].line_num = 0;
-	}
+    int ret;
+    char buf;
+    int line_n = 0;
+    char ending;
+    int res;
 
-	printf("Enter line number: \n");
-	scanf("%d", &line_n);
-	if (line_n == 0) {
-		return 0;
-	}
+    struct newLines lines[1024];
+    for (int i = 0; i < 1024; ++i) {
+        lines[i].line_len = 0;
+        lines[i].line_offset = -1;
+        lines[i].line_num = 0;
+    }
 
-	int i = 0;
-	/*seeking new line symbols*/
-	while (read(file, &buf, 1)) {
-		if (buf == '\n') {
-			lines[i].line_offset = lseek(file, 0L, SEEK_CUR);
-			++i;
+    printf("Enter line number: \n");
+    /* watch stdin for input */
+    fds[0].fd = 0;
+    fds[0].events = POLLIN;
+
+    for(; ;) {
+	    while((ret = poll(fds, 1, TIMEOUT * 1000))) {
+		if(res = scanf("%d%c", &line_n, &ending)){
+		} else {
+		    perror("Error: Invalid symbol\n");
+		    close(file);
+		    return -1;
 		}
-	}
+		break;
+	    }
 
-	/*getting line length*/
-	for (int i = 0; i < 1024 && lines[i].line_offset != -1; ++i) {
+	    if (ret == -1) {
+		perror("Error: poll\n");
+		return -1;
+	    }
+	    else if (ret == 0) {
+		printf("Sorry, %d seconds expired. Please, try again\n", TIMEOUT);
+		system("cat input.txt");
+		return 0;
+	    }
+	    else {
+		if (fds[0].revents & POLLIN)
+		    fds[0].revents = 0;
+		if (line_n == 0) {
+		    return 0;
+		}
+	    }
+
+	    int i = 0;
+	    /*seeking new line symbols*/
+	    while (read(file, &buf, 1)) {
+		if (buf == '\n') {
+		    lines[i].line_offset = lseek(file, 0L, SEEK_CUR);
+		    ++i;
+		}
+	    }
+
+	    int offset = 0;
+	    /*getting line length*/
+	    for (int i = 0; i < 1024 && lines[i].line_offset != -1; ++i) {
 		lines[i].line_len = lines[i].line_offset - offset - 1;
 		lines[i].line_num = i + 1;
 		offset = lines[i].line_offset;
-	}
+	    }
 
-	int num_of_lines = 0;
-	for (int i = 0; i < 1024 && lines[i].line_num != 0; ++i) {
+	    int num_of_lines = 0;
+	    for (int i = 0; i < 1024 && lines[i].line_num != 0; ++i) {
 		++num_of_lines;
-	}
+	    }
 
-	if (line_n > num_of_lines) {
+	    if (line_n > num_of_lines) {
 		perror("Error : Line number out of range\n");
-		return EXIT_FAILURE;
-	}
+		close(file);
+		return -1;
+	    }
 
-	/*for (int i = 0; i < 1024 && lines[i].line_offset != -1; ++i) {
+	    /*for (int i = 0; i < 1024 && lines[i].line_offset != -1; ++i) {
 		printf("%d: offset %d, line_len %d\n", lines[i].line_num, lines[i].line_offset, lines[i].line_len);
-	}*/
+	    }*/
 
-	char line[1024];
-	for (int i = 0; i < 1024; ++i) {
+	    char line[1024];
+	    for (int i = 0; i < 1024; ++i) {
 		line[i] = '\0';
-	}
+	    }
 
-	lseek(file, 0L, SEEK_SET);
-	lseek(file, lines[line_n - 1].line_offset - lines[line_n - 1].line_len - 1, SEEK_SET);
-	read(file, line, lines[line_n - 1].line_len);
-	
-	for (int i = 0; i < 1024 && line[i] != '\0'; ++i) {
+	    lseek(file, 0L, SEEK_SET);
+	    lseek(file, lines[line_n - 1].line_offset - lines[line_n - 1].line_len - 1, SEEK_SET);
+	    read(file, line, lines[line_n - 1].line_len);
+
+	    for (int i = 0; i < 1024 && line[i] != '\0'; ++i) {
 		printf("%c", line[i]);
-	}
-	printf("\n");
+	    }
+	    printf("\n");
+    }
 
-	close(file);
-	return 0;
+    close(file);
+    return 0;
 }
