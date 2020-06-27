b7ffc77e59f440c218b13e52b33c2427ba3209fa
Виктория <55043240+zzomg@users.noreply.github.com>
2020-06-27 15:39:05 +0700

    Update lab7.c

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -17,9 +17,9 @@ struct fileMapping {
 
 struct newLines
 {
-	int line_offset;
-	int line_len;
-	int line_num;
+    int line_offset;
+    int line_len;
+    int line_num;
 };
 
 int main()
@@ -27,15 +27,19 @@ int main()
     struct stat st;
     struct pollfd fds[1];
 
-	int ret;
-	char buf;
-	int line_n = -1;
+    int ret;
+    char buf;
+    //int line_n = -1;
+    int line_n = 0;
+    char ending;
+    int res;
+
 
     int file = open("input.txt", O_RDONLY);
-	if(file < 0) {
-		perror("Error: File opening failed\n");
-		return -1;
-	}
+    if(file < 0) {
+        perror("Error: File opening failed\n");
+        return -1;
+    }
 
     if(fstat(file, &st) < 0) {
         perror("Error: Couldn't obtain information about file\n");
@@ -56,14 +60,14 @@ int main()
     }
 
     struct newLines lines[STOR_SIZE];
-	for (int i = 0; i < STOR_SIZE; ++i) {
-		lines[i].line_len = 0;
-		lines[i].line_offset = -1;
-		lines[i].line_num = 0;
-	}
+    for (int i = 0; i < STOR_SIZE; ++i) {
+        lines[i].line_len = 0;
+        lines[i].line_offset = -1;
+        lines[i].line_num = 0;
+    }
 
     fds[0].fd = 0;
-	fds[0].events = POLLIN;
+    fds[0].events = POLLIN;
 
     struct fileMapping* mapping = (struct fileMapping*)malloc(sizeof(struct fileMapping));
 
@@ -84,60 +88,62 @@ int main()
         }
     }
 
-    /*for(int i = 0; i < STOR_SIZE && lines[i].line_offset != -1; ++i) {
-        printf("line_num: %d | line_len: %d | line_offset: %d\n", 
-            lines[i].line_num,
-            lines[i].line_len,
-            lines[i].line_offset);
-    }*/
-
     int num_of_lines = 0;
-	for (int i = 0; i < STOR_SIZE && lines[i].line_num != 0; ++i) {
-		++num_of_lines;
-	}
+    for (int i = 0; i < STOR_SIZE && lines[i].line_num != 0; ++i) {
+        ++num_of_lines;
+    }
 
     printf("Enter line number: \n");
-	/* watch stdin for input */
-	fds[0].fd = 0;
-	fds[0].events = POLLIN;
+    /* watch stdin for input */
+    fds[0].fd = 0;
+    fds[0].events = POLLIN;
 
-	while((ret = poll(fds, 1, TIMEOUT * 1000))) {
-	    if(scanf("%d", &line_n)) break;
+    for(; ;) {
+        while((ret = poll(fds, 1, TIMEOUT * 1000))) {
+                if(res = scanf("%d%c", &line_n, &ending)) { 
+            } else {
+                perror("Error: Invalid symbol\n");
+                close(file);
+                return -1;
+            }
+            break;
+        }
+
+
+        if (ret == -1) {
+            perror("Error: poll\n");
+            return -1;
+        }
+        else if (ret == 0) {
+            printf("Sorry, %d seconds expired. Please, try again.\n", TIMEOUT);
+            printf("Writing output:\n");
+            system("cat input.txt");
+            return 0;
+        }
+        else {
+            if (fds[0].revents & POLLIN)
+                fds[0].revents = 0;
+            if (line_n == 0) {
+                return 0;
+            }
+        }
+
+        if (line_n > num_of_lines) {
+            perror("Error : Line number out of range\n");
+            close(file);
+            return -1;
+        }
+
+        char line[1024];
+        for (int i = 0; i < 1024; ++i) {
+            line[i] = '\0';
+        }
+
+        for(int i = 0; i < lines[line_n - 1].line_len; ++i) {
+            printf("%c", mapping->dataPtr[lines[line_n - 1].line_offset + i]);
+        }
+        printf("\n");
     }
-
-	if (ret == -1) {
-		perror("Error: poll\n");
-		return -1;
-	}
-	else if (ret == 0) {
-		printf("Sorry, %d seconds expired. Please, try again.\n", TIMEOUT);
-        printf("Writing output:\n");
-		system("cat input.txt");
-		return 0;
-	}
-	else {
-		if (fds[0].revents & POLLIN)
-			fds[0].revents = 0;
-		if (line_n == 0) {
-			return 0;
-		}
-	}
-
-    if (line_n > num_of_lines) {
-		perror("Error : Line number out of range\n");
-		close(file);
-		return -1;
-	}
-
-    char line[1024];
-	for (int i = 0; i < 1024; ++i) {
-		line[i] = '\0';
-	}
-
-    for(int i = 0; i < lines[line_n - 1].line_len; ++i) {
-        printf("%c", mapping->dataPtr[lines[line_n - 1].line_offset + i]);
-    }
-    printf("\n");
 
     munmap(mapping->dataPtr, mapping->fsize);
     close(mapping->fd);
